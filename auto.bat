ec6d13d9ce996e2aa1134e74b28843cdcacc2b8f
SunIBAS <1134098699@qq.com>
2020-02-14 17:20:48 +0800

    2020/02/14 周五 17:20:48.67 update data

--- auto.bat
+++ auto.bat
@@ -1,8 +1,8 @@
 :: AutoRunBatFile.exe 1800000 auto.bat
 node autoRefleshAndDear.js
 node getChinaChange.js
-node Iwant/getAllMoreThanThou.js
-node Iwant/getIwant.js
+:: node Iwant/getAllMoreThanThou.js
+:: node Iwant/getIwant.js
 node extend/web/makeOneMapJson.js
 
 git add *
